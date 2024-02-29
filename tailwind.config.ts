f9b313c0e5b853f8d9c9d035273d5d439bf71ffe
Tanaca <jtanacar@gmail.com>
2024-02-29 10:35:44 -0300

    style: add path components in tailwind and re-add color moss-500

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,7 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
