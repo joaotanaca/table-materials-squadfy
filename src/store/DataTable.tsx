435a530f7d1ae81c6cc30d2c036e7c8b135fa8c3
Tanaca <jtanacar@gmail.com>
2024-03-01 16:58:28 -0300

    feat: create state formated to total

--- src/store/DataTable.tsx
+++ src/store/DataTable.tsx
@@ -9,6 +9,14 @@ import { TableHeader } from '../components/organisms/DataTable';
 export const rowsAtom = atom([] as Rows);
 export const headersAtom = atom([] as TableHeader[]);
 export const totalAtom = atom(0);
+export const totalFormatedAtom = atom((get) =>
+  get(totalAtom).toLocaleString('pt-BR', {
+    currency: 'BRL',
+    style: 'currency',
+    maximumFractionDigits: 2,
+    minimumFractionDigits: 2,
+  }),
+);
 export const searchAtom = atom('');
 
 export const DataTableStore = createStore();
