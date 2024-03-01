2095556acce2cc6eaf9889bccbc873d3b3a0496b
Tanaca <jtanacar@gmail.com>
2024-03-01 15:17:32 -0300

    chore: add new props to database.json

--- database/index.js
+++ database/index.js
@@ -13,15 +13,16 @@ for (let index = 0; index < 1000; index++) {
   const name = Math.floor(Math.random() * 4);
   const type = Math.floor(Math.random() * 4);
   const brand = Math.floor(Math.random() * 3);
-  const minimum_quantity = Math.floor(Math.random() * 6);
+  const minimum_quantity = Math.floor(Math.random() * 6) + 1;
   json.push({
     id: v4(),
     name: PRODUCT_MOCK.name[name],
     type: PRODUCT_MOCK.type[type],
     brand: PRODUCT_MOCK.brand[brand],
-    price: '150,00',
-    unit_quantity: 0,
+    price: 150.0,
     minimum_quantity,
+    unit_quantity: 0,
+    subtotal: 0,
   });
 }
 try {
