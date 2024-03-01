d1f26af34f6a0f34cf97832741726adef982bc8a
Tanaca <jtanacar@gmail.com>
2024-03-01 16:57:25 -0300

    style: add icons in input component

--- src/components/atom/Input.tsx
+++ src/components/atom/Input.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import Icon, { TIcons } from './Icon';
 
 type Props = React.InputHTMLAttributes<HTMLInputElement> & {
@@ -9,10 +9,20 @@ type Props = React.InputHTMLAttributes<HTMLInputElement> & {
 };
 
 const Input = ({ containerClassName = '', className = '', icon, ...props }: Props) => {
+  const ref = useRef<HTMLInputElement>(null);
+  const focus = useCallback(() => ref?.current?.focus(), []);
   return (
-    <div className={`flex gap-3 py-3 px-4 ${containerClassName}`}>
-      {icon ? <Icon name={icon} size={24} /> : null}
-      <input className={`${className}`} {...props} />
+    <div className={`border-moss-300 border-1 relative ${containerClassName}`}>
+      {icon ? (
+        <div onClick={focus}>
+          <Icon className="absolute left-4 top-1/2 -translate-y-1/2 cursor-pointer" name={icon} size={24} />
+        </div>
+      ) : null}
+      <input
+        ref={ref}
+        className={`placeholder:text-moss-500 text-moss-500 flex gap-3 py-3 outline-none ${icon ? ' pl-12 pr-4' : 'px-4'} ${className}`}
+        {...props}
+      />
     </div>
   );
 };
