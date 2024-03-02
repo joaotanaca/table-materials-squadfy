86dcff17cc8c8b633e81b6107c0907960756efc6
Tanaca <jtanacar@gmail.com>
2024-03-02 00:28:25 -0300

    style: fix observer paths

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,8 +4,9 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/constants/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
