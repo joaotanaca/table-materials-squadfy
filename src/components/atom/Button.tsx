36898c1b0d7dcde1fe3313fb38a00693c7e71612
Tanaca <jtanacar@gmail.com>
2024-03-04 01:08:36 -0300

    perf: add memo in Button component

--- src/components/atom/Button.tsx
+++ src/components/atom/Button.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { memo } from 'react';
 
 const colors = {
   success: 'bg-success text-white',
@@ -27,4 +27,4 @@ const Button = ({ color = 'success', format = 'default', className = '', childre
   );
 };
 
-export default Button;
+export default memo(Button);
